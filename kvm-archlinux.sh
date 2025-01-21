c7495d92bc5da5f700d2505b566d764affb777c0
divyansh <dpi0.dev@proton.me>
2025-01-21 17:32:59 +0530

    Update kvm-archlinux.sh

--- kvm-archlinux.sh
+++ kvm-archlinux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# enables automatic exit from a script when a command fails
+# Enables automatic exit from a script when a command fails
 set -e
 
 if [[ "$1" == "chroot" ]]; then
@@ -21,151 +21,148 @@ export USERGROUP="wheel"
 export USER_PASSWORD="user"
 # export ROOT_PASSWORD="root"
 
-# Color definitions
-RED='\033[31m'
-GREEN='\033[32m'
-YELLOW='\033[33m'
-BLUE='\033[34m'
-MAGENTA='\033[35m'
-CYAN='\033[36m'
-WHITE='\033[37m'
-RESET='\033[0m'
+# Helper function to print messages
+print_() {
+  echo ">>> $1"
+}
+
+# Helper function to run commands with error handling
+run_() {
+  print_ "Running: $1"
+  eval "$1" || { echo "Error: Command failed"; exit 1; }
+}
 
 check_root() {
   if [ "$(id -u)" -ne 0 ]; then
-    echo -e "${RED}This script must be run as root${RESET}" 1>&2
+    echo "This script must be run as root" 1>&2
     exit 1
   fi
 }
 
 check_uefi() {
   if [ ! -d /sys/firmware/efi ]; then
-    echo -e "${YELLOW}Warning: This script is intended for UEFI platforms${RESET}" 1>&2
+    echo "Warning: This script is intended for UEFI platforms" 1>&2
     exit 1
   fi
 }
 
 setup_preliminary() {
-  echo -e "${GREEN}>>> Preliminary setup...${RESET}"
-  setfont "$FONT"
-  loadkeys "$KEYMAP"
-  timedatectl set-timezone "$TIMEZONE"
-  timedatectl set-ntp true
-  cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-  reflector --verbose -c India -l 10 -p https --sort rate --save /etc/pacman.d/mirrorlist
-  echo -e "${GREEN}Preliminary done!${RESET}"
+  print_ "Preliminary setup..."
+  run_ "setfont $FONT"
+  run_ "loadkeys $KEYMAP"
+  run_ "timedatectl set-timezone $TIMEZONE"
+  run_ "timedatectl set-ntp true"
+  run_ "cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak"
+  run_ "reflector --verbose -c India -l 10 -p https --sort rate --save /etc/pacman.d/mirrorlist"
+  print_ "Preliminary setup done!"
 }
 
 create_partitions() {
-  echo -e "${CYAN}>>> Creating Partitions...${RESET}"
-  parted -s "$DISK" mklabel gpt
-  parted -s "$DISK" mkpart primary fat32 1MiB 256MiB
-  parted -s "$DISK" set 1 esp on
-  parted -s "$DISK" mkpart primary ext4 256MiB 1024MiB
-  parted -s "$DISK" set 2 bls_boot on
-  parted -s "$DISK" mkpart primary ext4 1024MiB 100%
-  echo -e "${GREEN}Partitions created!${RESET}"
+  print_ "Creating partitions..."
+  run_ "parted -s $DISK mklabel gpt"
+  run_ "parted -s $DISK mkpart primary fat32 1MiB 256MiB"
+  run_ "parted -s $DISK set 1 esp on"
+  run_ "parted -s $DISK mkpart primary ext4 256MiB 1024MiB"
+  run_ "parted -s $DISK set 2 bls_boot on"
+  run_ "parted -s $DISK mkpart primary ext4 1024MiB 100%"
+  print_ "Partitions created!"
 }
 
 format_partitions() {
-  echo -e "${CYAN}>>> Formatting Partitions...${RESET}"
-  mkfs.fat -F32 "$DISK"1
-  mkfs.fat -F32 "$DISK"2
-  mkfs.ext4 "$DISK"3
-  echo -e "${GREEN}Partitions formatted!${RESET}"
+  print_ "Formatting partitions..."
+  run_ "mkfs.fat -F32 ${DISK}1"
+  run_ "mkfs.fat -F32 ${DISK}2"
+  run_ "mkfs.ext4 ${DISK}3"
+  print_ "Partitions formatted!"
 }
 
 mount_partitions() {
-  echo -e "${CYAN}>>> Mounting Partitions...${RESET}"
-  mount "$DISK"3 /mnt
-  mkdir /mnt/efi /mnt/boot
-  mount "$DISK"1 /mnt/efi
-  mount "$DISK"2 /mnt/boot
-  echo -e "${GREEN}Partitions mounted!${RESET}"
+  print_ "Mounting partitions..."
+  run_ "mount ${DISK}3 /mnt"
+  run_ "mkdir /mnt/efi /mnt/boot"
+  run_ "mount ${DISK}1 /mnt/efi"
+  run_ "mount ${DISK}2 /mnt/boot"
+  print_ "Partitions mounted!"
 }
 
 pacstrap_install() {
-  echo -e "${CYAN}>>> Pacstrap${RESET}"
-  pacstrap /mnt base linux vim sudo less intel-ucode
-  echo -e "${GREEN}Packages installed!${RESET}"
+  print_ "Installing base system..."
+  run_ "pacstrap /mnt base linux vim sudo less intel-ucode"
+  print_ "Base system installed!"
 }
 
 fstab() {
-  echo -e "${GREEN}>>> Writing fstab...${RESET}"
-  genfstab -U /mnt >> /mnt/etc/fstab
-  echo -e "${GREEN}fstab written!${RESET}"
+  print_ "Generating fstab..."
+  run_ "genfstab -U /mnt >> /mnt/etc/fstab"
+  print_ "fstab generated!"
 }
 
 timezone() {
-  echo -e "${CYAN}>>> Seting timezone...${RESET}"
-  ln -sf /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
-  hwclock -w
-  echo -e "${GREEN}Timezone set!${RESET}"
+  print_ "Setting timezone..."
+  run_ "ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime"
+  run_ "hwclock -w"
+  print_ "Timezone set!"
 }
 
 set_locale() {
-  echo -e "${CYAN}>>> Setting locale...${RESET}"
-  echo "$LOCALE_LANG_COMPLETE" >> /etc/locale.gen
-  locale-gen
-  echo "LANG=$LOCALE_LANG" >> /etc/locale.conf
-  echo "KEYMAP=$KEYMAP" >> /etc/vconsole.conf
-  echo "FONT=$FONT" >> /etc/vconsole.conf
-  echo "$HOSTNAME" >> /etc/hostname
-  echo -e "${GREEN}Locale set!${RESET}"
+  print_ "Setting locale..."
+  run_ "echo $LOCALE_LANG_COMPLETE >> /etc/locale.gen"
+  run_ "locale-gen"
+  run_ "echo LANG=$LOCALE_LANG >> /etc/locale.conf"
+  run_ "echo KEYMAP=$KEYMAP >> /etc/vconsole.conf"
+  run_ "echo FONT=$FONT >> /etc/vconsole.conf"
+  run_ "echo $HOSTNAME >> /etc/hostname"
+  print_ "Locale set!"
 }
 
 create_user() {
-  echo -e "${CYAN}>>> Creating user $USERNAME...${RESET}"
-  useradd -mg "$USERGROUP" "$USERNAME"
-  echo "$USERNAME:$USER_PASSWORD" | chpasswd
-  echo "%$USERGROUP ALL=(ALL:ALL) ALL" >> /etc/sudoers
-  echo -e "${GREEN}User $USERNAME created!${RESET}"
+  print_ "Creating user $USERNAME..."
+  run_ "useradd -mg $USERGROUP $USERNAME"
+  run_ "echo $USERNAME:$USER_PASSWORD | chpasswd"
+  run_ "echo %$USERGROUP ALL=(ALL:ALL) ALL >> /etc/sudoers"
+  print_ "User $USERNAME created!"
 }
 
 pacman_fix() {
-  echo -e "${CYAN}>>> Configuring Pacman...${RESET}"
-  sed -i 's/^#Color/Color/' /etc/pacman.conf
-  sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
-  sed -i '/^Color/a ILoveCandy' /etc/pacman.conf
-  echo -e "${GREEN}Pacman configured!${RESET}"
+  print_ "Configuring Pacman..."
+  run_ "sed -i 's/^#Color/Color/' /etc/pacman.conf"
+  run_ "sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf"
+  run_ "sed -i '/^Color/a ILoveCandy' /etc/pacman.conf"
+  print_ "Pacman configured!"
 }
 
 more_packages() {
-  echo -e "${CYAN}>>> Installing additional packages...${RESET}"
-  pacman -S --noconfirm git openssh pacman-contrib networkmanager
-  echo -e "${GREEN}Additional packages installed!${RESET}"
+  print_ "Installing additional packages..."
+  run_ "pacman -S --noconfirm git openssh pacman-contrib networkmanager"
+  print_ "Additional packages installed!"
 }
 
 systemd_services() {
-  echo -e "${CYAN}>>> Enabling systemd services...${RESET}"
-  systemctl enable fstrim.timer paccache.timer NetworkManager sshd
-  echo -e "${GREEN}Systemd services enabled!${RESET}"
+  print_ "Enabling systemd services..."
+  run_ "systemctl enable fstrim.timer paccache.timer NetworkManager sshd"
+  print_ "Systemd services enabled!"
 }
 
 bootloader_setup() {
-  echo -e "${CYAN}>>> Installing bootloader...${RESET}"
-  bootctl --esp-path=/efi --boot-path=/boot install
+  print_ "Installing bootloader..."
+  run_ "bootctl --esp-path=/efi --boot-path=/boot install"
   
-  echo -e "${CYAN}>>> Configuring bootloader settings...${RESET}"
-  {
-    echo timeout 0
-    echo default arch.conf
-    echo console-mode max
-    echo editor no
-  } >>/efi/loader/loader.conf
+  print_ "Configuring bootloader settings..."
+  run_ "echo timeout 0 >> /efi/loader/loader.conf"
+  run_ "echo default arch.conf >> /efi/loader/loader.conf"
+  run_ "echo console-mode max >> /efi/loader/loader.conf"
+  run_ "echo editor no >> /efi/loader/loader.conf"
   
-  echo -e "${CYAN}>>> Getting UUID for root partition...${RESET}"
-  UUID=$(blkid -s UUID -o value "$DISK"3)
+  print_ "Getting UUID for root partition..."
+  UUID=$(blkid -s UUID -o value ${DISK}3)
 
-  echo -e "${CYAN}>>> Configuring bootloader entry for Arch Linux...${RESET}"
-  {
-    echo title Arch Linux
-    echo linux /vmlinuz-linux
-    echo initrd /initramfs-linux.img
-    echo options root=UUID="$UUID" rw
-  } >>/boot/loader/entries/arch.conf
+  print_ "Configuring bootloader entry for Arch Linux..."
+  run_ "echo title Arch Linux >> /boot/loader/entries/arch.conf"
+  run_ "echo linux /vmlinuz-linux >> /boot/loader/entries/arch.conf"
+  run_ "echo initrd /initramfs-linux.img >> /boot/loader/entries/arch.conf"
+  run_ "echo options root=UUID=$UUID rw >> /boot/loader/entries/arch.conf"
   
-  echo -e "${GREEN}>>> Bootloader installed and configured!${RESET}"
+  print_ "Bootloader installed and configured!"
 }
 
 base_chroot() {
@@ -191,9 +188,9 @@ base() {
   cp "$0" /mnt/root/install.sh
   arch-chroot /mnt /bin/bash /root/install.sh chroot
 
-  echo -e "${GREEN}Base system setup complete!${RESET}"
+  print_ "Base system setup complete!"
 
-  echo -e "${GREEN}>>> Leaving chroot and rebooting"
+  print_ "Leaving chroot and rebooting..."
   
   umount -R /mnt
   reboot
@@ -207,4 +204,5 @@ main() {
   fi
 }
 
-main; exit
+main
+exit
