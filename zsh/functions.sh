e537bfed49c85aaaf7b5490ab9e3b5d06b508590
dpi0 <dpi0.dev@proton.me>
2025-02-20 17:30:10 +0530

    Remove yazi config & Update zsh/functions.sh

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -1,245 +1,296 @@
 lfs() {
-    sudo yazi
+  sudo yazi
 }
 
 copydir() {
-    # Check if wl-copy is installed
-    if command -v wl-copy &> /dev/null; then
-        print -n $PWD | wl-copy
-    else
-        echo "wl-copy is not installed. Cannot copy current directory."
-    fi
+  # Check if wl-copy is installed
+  if command -v wl-copy &> /dev/null; then
+    print -n $PWD | wl-copy
+  else
+    echo "wl-copy is not installed. Cannot copy current directory."
+  fi
 }
 
 copylastcommand() {
-    # Check if wl-copy is installed
-    if command -v wl-copy &> /dev/null; then
-        fc -ln -1 | tr -d '\n' | wl-copy
-    else
-        echo "wl-copy is not installed. Cannot copy last command."
-    fi
+  # Check if wl-copy is installed
+  if command -v wl-copy &> /dev/null; then
+    fc -ln -1 | tr -d '\n' | wl-copy
+  else
+    echo "wl-copy is not installed. Cannot copy last command."
+  fi
 }
 
 copybuffer() {
-    # You've already implemented the check here!
-    if command -v wl-copy &>/dev/null; then
-        echo "$BUFFER" | wl-copy
-    else
-        echo "Error! Couldn't copy current line. wl-copy not present"
-    fi
+  # You've already implemented the check here!
+  if command -v wl-copy &> /dev/null; then
+    echo "$BUFFER" | wl-copy
+  else
+    echo "Error! Couldn't copy current line. wl-copy not present"
+  fi
 }
 
 count_files() {
-    local dir="${1:-$(pwd)}"
-    local group=false
+  local dir="${1:-$(pwd)}"
+  local group=false
 
-    if [ "$1" = "-g" ] || [ "$1" = "--group" ]; then
-        group=true
-        dir="${2:-$(pwd)}"
-    fi
+  if [ "$1" = "-g" ] || [ "$1" = "--group" ]; then
+    group=true
+    dir="${2:-$(pwd)}"
+  fi
 
-    if $group; then
-        fd -H -t f . "$dir" | awk -F. '{ext = $NF; if (ext != $0) {count[ext]++}} END {for (ext in count) print ext, count[ext]}'
-    else
-        fd -H . "$dir" | wc -l
-    fi
+  if $group; then
+    fd -H -t f . "$dir" | awk -F. '{ext = $NF; if (ext != $0) {count[ext]++}} END {for (ext in count) print ext, count[ext]}'
+  else
+    fd -H . "$dir" | wc -l
+  fi
 }
 
-speedtest_curl(){
-	curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -
+speedtest_curl() {
+  curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -
 }
 
 f() {
-    search_term="$1"
+  search_term="$1"
 
-    SELECTED_FILE=$(
-        fzf \
-            --query="$search_term" \
-            --exact \
-            --extended \
-            --preview="bat --style=numbers --color=always --line-range :500 {}" \
-            --preview-window="right:50%" \
-            --ansi
-    )
+  SELECTED_FILE=$(
+    fd --type f --hidden --follow --color=always \
+      --exclude ".local" \
+      --exclude ".rustup" \
+      --exclude "node_modules" \
+      --exclude ".cargo" \
+      --exclude ".continue" \
+      --exclude ".cargo" \
+      --exclude ".mozilla" \
+      --exclude "go/pkg/mod" \
+      --exclude "Code/User" \
+      --exclude ".git" \
+      --exclude ".npm" \
+      --exclude ".cache" |
+      fzf \
+        --query="$search_term" \
+        --exact \
+        --extended \
+        --preview="bat --style=numbers --color=always --line-range :500 {}" \
+        --preview-window="right:50%" \
+        --ansi
+  )
 
-    if [ -n "$SELECTED_FILE" ]; then
-        file_extension="${SELECTED_FILE##*.}"
-
-        case "$file_extension" in
-            "pdf")
-                evince "$SELECTED_FILE"
-                ;;
-            "txt" | "py" | "rs" | "go" | "md")
-                if command -v nvim &> /dev/null; then
-                    nvim "$SELECTED_FILE"
-                else
-                    vim "$SELECTED_FILE"
-                fi
-                ;;
-            *)
-                if command -v nvim &> /dev/null; then
-                    nvim "$SELECTED_FILE"
-                else
-                    vim "$SELECTED_FILE"
-                fi
-                ;;
-        esac
-
-        # Check if wl-copy is installed
-        if command -v wl-copy &> /dev/null; then
-            # Print the selected file value to clipboard
-            echo "$SELECTED_FILE" | wl-copy
-        fi
-
-        # Print the selected file value to stdout
-        echo "$SELECTED_FILE" | wl-copy
-        echo "$SELECTED_FILE"
-    fi
-}
-
-s() {
-    search_term="$1"
-
-        SELECTED_FILE=$(
-            fd --type f --hidden --follow --search-path /home/dpi0 --exclude '**/.cache' --exclude '**/.Trash-1000' --color=always | fzf \
-                --query="$search_term" \
-                --exact \
-                --extended \
-                --preview="bat --style=numbers --color=always --line-range :500 {}" \
-                --preview-window="right:50%" \
-                --ansi
-        )
-
-if [ -n "$SELECTED_FILE" ]; then
+  if [ -n "$SELECTED_FILE" ]; then
     file_extension="${SELECTED_FILE##*.}"
 
     case "$file_extension" in
-        "pdf")
-            evince "$SELECTED_FILE"
-            ;;
-        "png" | "jpg" | "jpeg")
-            loupe "$SELECTED_FILE"
-            ;;
-        "mp4" | "mkv" | "gif")
-            mpv "$SELECTED_FILE"
-            ;;
-        "txt" | "py" | "rs" | "go" | "md")
-            if command -v nvim &> /dev/null; then
-                nvim "$SELECTED_FILE"
-            else
-                vim "$SELECTED_FILE"
-            fi
-            ;;
-        *)
-            if command -v nvim &> /dev/null; then
-                nvim "$SELECTED_FILE"
-            else
-                vim "$SELECTED_FILE"
-            fi
-            ;;
+      "pdf")
+        evince "$SELECTED_FILE"
+        ;;
+      "txt" | "py" | "rs" | "go" | "md")
+        if command -v nvim &> /dev/null; then
+          nvim "$SELECTED_FILE"
+        else
+          vim "$SELECTED_FILE"
+        fi
+        ;;
+      *)
+        if command -v nvim &> /dev/null; then
+          nvim "$SELECTED_FILE"
+        else
+          vim "$SELECTED_FILE"
+        fi
+        ;;
     esac
 
-        # Print the selected file value to stdout
-        echo "$SELECTED_FILE" | wl-copy
-        echo "$SELECTED_FILE"
+    # Check if wl-copy is installed
+    if command -v wl-copy &> /dev/null; then
+      # Print the selected file value to clipboard
+      echo "$SELECTED_FILE" | wl-copy
     fi
+
+    # Print the selected file value to stdout
+    echo "$SELECTED_FILE"
+  fi
+}
+
+s() {
+  search_term="$1"
+
+  SELECTED_FILE=$(
+    fd --type f --hidden --follow --color=always \
+      --exclude ".local" \
+      --exclude ".rustup" \
+      --exclude "node_modules" \
+      --exclude ".cargo" \
+      --exclude ".continue" \
+      --exclude ".cargo" \
+      --exclude ".mozilla" \
+      --exclude "go/pkg/mod" \
+      --exclude "Code/User" \
+      --exclude ".git" \
+      --exclude ".npm" \
+      --exclude ".cache" |
+      fzf \
+        --query="$search_term" \
+        --exact \
+        --extended \
+        --preview="bat --style=numbers --color=always --line-range :500 {}" \
+        --preview-window="right:50%" \
+        --ansi
+  )
+
+  if [ -n "$SELECTED_FILE" ]; then
+    file_extension="${SELECTED_FILE##*.}"
+
+    case "$file_extension" in
+      "pdf")
+        evince "$SELECTED_FILE"
+        ;;
+      "png" | "jpg" | "jpeg")
+        loupe "$SELECTED_FILE"
+        ;;
+      "mp4" | "mkv" | "gif")
+        mpv "$SELECTED_FILE"
+        ;;
+      "txt" | "py" | "rs" | "go" | "md")
+        if command -v nvim &> /dev/null; then
+          nvim "$SELECTED_FILE"
+        else
+          vim "$SELECTED_FILE"
+        fi
+        ;;
+      *)
+        if command -v nvim &> /dev/null; then
+          nvim "$SELECTED_FILE"
+        else
+          vim "$SELECTED_FILE"
+        fi
+        ;;
+    esac
+
+    # Print the selected file value to stdout
+    echo "$SELECTED_FILE" | wl-copy
+    echo "$SELECTED_FILE"
+  fi
 }
 
 # cd into dir
 ff() {
-	local selected_file
-	selected_file=$(
-	fzf \
-		--query="$search_term" \
-		--exact \
-		--extended \
-		--preview="bat --style=numbers --color=always --line-range :500 {}" \
-		--preview-window="right:50%" \
-		--ansi
-	)
-	if [[ -n $selected_file ]]; then
-		local parent_dir
-		parent_dir=$(dirname "$selected_file")
-		cd "$parent_dir" || echo "Failed to change directory"
-	else
-		echo "No file selected"
-	fi
+  local selected_file
+  selected_file=$(
+    fd --type f --hidden --follow --color=always \
+      --exclude ".local" \
+      --exclude ".rustup" \
+      --exclude "node_modules" \
+      --exclude ".cargo" \
+      --exclude ".continue" \
+      --exclude ".cargo" \
+      --exclude ".mozilla" \
+      --exclude "go/pkg/mod" \
+      --exclude "Code/User" \
+      --exclude ".git" \
+      --exclude ".npm" \
+      --exclude ".cache" |
+      fzf \
+        --query="$search_term" \
+        --exact \
+        --extended \
+        --preview="bat --style=numbers --color=always --line-range :500 {}" \
+        --preview-window="right:50%" \
+        --ansi
+  )
+  if [[ -n $selected_file ]]; then
+    local parent_dir
+    parent_dir=$(dirname "$selected_file")
+    cd "$parent_dir" || echo "Failed to change directory"
+  else
+    echo "No file selected"
+  fi
 }
 
 ss() {
-    local search_term
-    search_term="$1"
+  local search_term
+  search_term="$1"
 
-    local selected_file
-    selected_file=$(
-        fd --type f --hidden --follow --search-path /home/dpi0 --exclude '**/.cache' --exclude '**/.Trash-1000' --color=always | fzf \
-            --query="$search_term" \
-            --exact \
-            --extended \
-            --preview="bat --style=numbers --color=always --line-range :500 {}" \
-            --preview-window="right:50%" \
-            --ansi
-    )
+  local selected_file
+  selected_file=$(
+    fd --type f --hidden --follow --color=always \
+      --exclude ".local" \
+      --exclude ".rustup" \
+      --exclude "node_modules" \
+      --exclude ".cargo" \
+      --exclude ".continue" \
+      --exclude ".cargo" \
+      --exclude ".mozilla" \
+      --exclude "go/pkg/mod" \
+      --exclude "Code/User" \
+      --exclude ".git" \
+      --exclude ".npm" \
+      --exclude ".cache" |
+      fzf \
+        --query="$search_term" \
+        --exact \
+        --extended \
+        --preview="bat --style=numbers --color=always --line-range :500 {}" \
+        --preview-window="right:50%" \
+        --ansi
+  )
 
-    if [[ -n $selected_file ]]; then
-        local parent_dir
-        parent_dir=$(dirname "$selected_file")
-        cd "$parent_dir" || echo "Failed to change directory"
-    else
-        echo "No file selected"
-    fi
+  if [[ -n $selected_file ]]; then
+    local parent_dir
+    parent_dir=$(dirname "$selected_file")
+    cd "$parent_dir" || echo "Failed to change directory"
+  else
+    echo "No file selected"
+  fi
 }
 
 remove_variable_from_history() {
-    local var_to_remove="$1"
-    local temp_file=$(mktemp)
+  local var_to_remove="$1"
+  local temp_file=$(mktemp)
 
-    # Use ripgrep to find lines containing the variable and list them
-    local found_lines=$(rg --color=always "$var_to_remove" ~/.zsh_history)
+  # Use ripgrep to find lines containing the variable and list them
+  local found_lines=$(rg --color=always "$var_to_remove" ~/.zsh_history)
 
-    if [[ -z "$found_lines" ]]; then
-        echo "No lines containing '$var_to_remove' found in ~/.zsh_history."
-        return
-    fi
+  if [[ -z "$found_lines" ]]; then
+    echo "No lines containing '$var_to_remove' found in ~/.zsh_history."
+    return
+  fi
 
-    # Display the found lines
-    echo "The following lines will be deleted:"
-    echo "$found_lines"
+  # Display the found lines
+  echo "The following lines will be deleted:"
+  echo "$found_lines"
 
-    # Use ripgrep to exclude lines containing the variable and write to the temporary file
-    rg -v "$var_to_remove" ~/.zsh_history > "$temp_file"
+  # Use ripgrep to exclude lines containing the variable and write to the temporary file
+  rg -v "$var_to_remove" ~/.zsh_history > "$temp_file"
 
-    # Overwrite the original history file with the cleaned version
-    mv "$temp_file" ~/.zsh_history
+  # Overwrite the original history file with the cleaned version
+  mv "$temp_file" ~/.zsh_history
 
-    echo "Deleted $(echo "$found_lines" | wc -l) lines containing '$var_to_remove'."
+  echo "Deleted $(echo "$found_lines" | wc -l) lines containing '$var_to_remove'."
 
-    # Reload the shell
-    exec zsh
+  # Reload the shell
+  exec zsh
 }
 
 fh() {
-    selected_command=$(history -E 1 | sort -k1,1nr | fzf | awk '{$1=""; $2=""; $3=""; print $0}' | sed 's/^[ \t]*//')
-    
-    # Check if wl-copy is installed
-    if command -v wl-copy &> /dev/null; then
-        echo "$selected_command" | wl-copy
-    else
-        echo "wl-copy is not installed. Cannot copy to clipboard."
-    fi
+  selected_command=$(history -E 1 | sort -k1,1nr | fzf | awk '{$1=""; $2=""; $3=""; print $0}' | sed 's/^[ \t]*//')
+
+  # Check if wl-copy is installed
+  if command -v wl-copy &> /dev/null; then
+    echo "$selected_command" | wl-copy
+  else
+    echo "wl-copy is not installed. Cannot copy to clipboard."
+  fi
 }
 
 finh() {
-	cat /var/log/pacman.log | grep -E "\[ALPM\] (installed|removed|upgraded|upgraded|downgraded)" | awk '{print $1, $2, $3, $4, $5, $6}' | sort -r | fzf
+  cat /var/log/pacman.log | grep -E "\[ALPM\] (installed|removed|upgraded|upgraded|downgraded)" | awk '{print $1, $2, $3, $4, $5, $6}' | sort -r | fzf
 }
 
 # search all installed packages
 fin() {
-	pacman -Qq | fzf --preview='pacman -Qi {}'
+  pacman -Qq | fzf --preview='pacman -Qi {}'
 }
 
 in() {
-	pacman -Slq | fzf -q "$1" -m --preview 'pacman -Si {1}' | xargs -ro pacman -S
+  pacman -Slq | fzf -q "$1" -m --preview 'pacman -Si {1}' | xargs -ro pacman -S
 }
 
 extract() {
@@ -248,16 +299,16 @@ extract() {
 
     # Create the target directory if it doesn't exist
     if [ ! -d "$target_dir" ]; then
-        mkdir -p "$target_dir"
+      mkdir -p "$target_dir"
     fi
 
     case "$1" in
-        *.zip) unzip "$1" -d "$target_dir" ;;
-        *.tar.gz) tar -xzvf "$1" -C "$target_dir" ;;
-        *.tar.xz) tar -xJf "$1" -C "$target_dir" ;;
-        *.tar.bz2) tar -xjvf "$1" -C "$target_dir" ;;
-        *.tar) tar -xvf "$1" -C "$target_dir" ;;
-        *) echo "Unsupported format" ;;
+      *.zip) unzip "$1" -d "$target_dir" ;;
+      *.tar.gz) tar -xzvf "$1" -C "$target_dir" ;;
+      *.tar.xz) tar -xJf "$1" -C "$target_dir" ;;
+      *.tar.bz2) tar -xjvf "$1" -C "$target_dir" ;;
+      *.tar) tar -xvf "$1" -C "$target_dir" ;;
+      *) echo "Unsupported format" ;;
     esac
 
   else
@@ -298,11 +349,11 @@ compress() {
 
 # launch-waybar(){
 #     CONFIG_FILES="$DOTFILES/waybar/config.jsonc $DOTFILES/waybar/style.css "
-#     
+#
 #     #$DOTFILES/waybar/config2.jsonc
-#     
+#
 #     trap "killall waybar" EXIT
-#     
+#
 #     while true; do
 #         waybar &
 #         inotifywait -e create,modify $CONFIG_FILES
@@ -327,7 +378,7 @@ note() {
 
 # Function to enable autocompletion for the `note` command
 _note_autocomplete() {
-  local cur_word="${1}"  # Current word typed
+  local cur_word="${1}" # Current word typed
   local notes_dir="$HOME/Notes/docs"
 
   # List all files and directories in the notes directory
