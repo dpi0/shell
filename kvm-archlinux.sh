314b9f1d378a7cb0be84799f0117279f94a452d0
divyansh <dpi0.dev@proton.me>
2025-03-06 13:16:46 +0530

    Remove colors from kvm-archlinux

--- kvm-archlinux.sh
+++ kvm-archlinux.sh
@@ -21,127 +21,117 @@ export USERGROUP="wheel"
 export USER_PASSWORD="user"
 # export ROOT_PASSWORD="root"
 
-# Color definitions
-RED='\033[31m'
-GREEN='\033[32m'
-YELLOW='\033[33m'
-BLUE='\033[34m'
-MAGENTA='\033[35m'
-CYAN='\033[36m'
-WHITE='\033[37m'
-RESET='\033[0m'
-
 check_root() {
   if [ "$(id -u)" -ne 0 ]; then
-    echo -e "${RED}This script must be run as root${RESET}" 1>&2
+    echo "This script must be run as root" 1>&2
     exit 1
   fi
 }
 
 check_uefi() {
   if [ ! -d /sys/firmware/efi ]; then
-    echo -e "${YELLOW}Warning: This script is intended for UEFI platforms${RESET}" 1>&2
+    echo "Warning: This script is intended for UEFI platforms" 1>&2
     exit 1
   fi
 }
 
 setup_preliminary() {
-  echo -e "${GREEN}>>> Preliminary setup: font, keyboard, time, date and package mirrors...${RESET}"
+  echo ">>> Preliminary setup: font, keyboard, time, date and package mirrors..."
   setfont "$FONT"
   loadkeys "$KEYMAP"
   timedatectl set-timezone "$TIMEZONE"
   timedatectl set-ntp true
   cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
   reflector --verbose -c India -l 10 -p https --sort rate --save /etc/pacman.d/mirrorlist
-  echo -e "${GREEN}Preliminary done!${RESET}"
+  echo "Preliminary done!"
 }
 
 create_partitions() {
-  echo -e "${CYAN}>>> Creating Partitions: /efi and /root...${RESET}"
+  echo ">>> Creating Partitions: /efi and /root..."
   parted -s "$DISK" mklabel gpt
   parted -s "$DISK" mkpart primary fat32 1MiB 512MiB
   parted -s "$DISK" set 1 esp on
   parted -s "$DISK" mkpart primary ext4 512MiB 100%
-  echo -e "${GREEN}Partitions created!${RESET}"
+  echo "Partitions created!"
 }
 
 format_partitions() {
-  echo -e "${CYAN}>>> Formatting Partitions...${RESET}"
+  echo ">>> Formatting Partitions..."
   mkfs.fat -F32 "$DISK"1
   mkfs.ext4 "$DISK"2
-  echo -e "${GREEN}Partitions formatted!${RESET}"
+  echo "Partitions formatted!"
 }
 
 mount_partitions() {
-  echo -e "${CYAN}>>> Mounting Partitions...${RESET}"
+  echo ">>> Mounting Partitions..."
   mount "$DISK"2 /mnt
   mount --mkdir "$DISK"1 /mnt/boot
-  echo -e "${GREEN}Partitions mounted!${RESET}"
+  echo "Partitions mounted!"
 }
 
 pacstrap_install() {
-  echo -e "${CYAN}>>> Pacstrap${RESET}"
+  echo ">>> Pacstrap"
   pacstrap /mnt base linux-lts vim sudo less intel-ucode
-  echo -e "${GREEN}Packages installed!${RESET}"
+  echo "Packages installed!"
 }
 
 fstab() {
-  echo -e "${GREEN}>>> Writing fstab...${RESET}"
+  echo ">>> Writing fstab..."
   genfstab -U /mnt >> /mnt/etc/fstab
-  echo -e "${GREEN}fstab written!${RESET}"
+  echo "fstab written!"
 }
 
 timezone() {
-  echo -e "${CYAN}>>> Seting timezone...${RESET}"
+  echo ">>> Setting timezone..."
   ln -sf /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
   hwclock -w
-  echo -e "${GREEN}Timezone set!${RESET}"
+  echo "Timezone set!"
 }
 
 set_locale() {
-  echo -e "${CYAN}>>> Setting locale...${RESET}"
+  echo ">>> Setting locale..."
   echo "$LOCALE_LANG_COMPLETE" >> /etc/locale.gen
   locale-gen
   echo "LANG=$LOCALE_LANG" >> /etc/locale.conf
   echo "KEYMAP=$KEYMAP" >> /etc/vconsole.conf
   echo "FONT=$FONT" >> /etc/vconsole.conf
   echo "$HOSTNAME" >> /etc/hostname
-  echo -e "${GREEN}Locale set!${RESET}"
+  echo "Locale set!"
 }
 
 create_user() {
-  echo -e "${CYAN}>>> Creating user $USERNAME...${RESET}"
+  echo ">>> Creating user $USERNAME..."
   useradd -mg "$USERGROUP" "$USERNAME"
   echo "$USERNAME:$USER_PASSWORD" | chpasswd
   echo "%$USERGROUP ALL=(ALL:ALL) ALL" >> /etc/sudoers
-  echo -e "${GREEN}User $USERNAME created!${RESET}"
+  echo "User $USERNAME created!"
 }
 
 pacman_fix() {
-  echo -e "${CYAN}>>> Configuring Pacman...${RESET}"
+  echo ">>> Configuring Pacman..."
   sed -i 's/^#Color/Color/' /etc/pacman.conf
   sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
   sed -i '/^Color/a ILoveCandy' /etc/pacman.conf
-  echo -e "${GREEN}Pacman configured!${RESET}"
+  echo "Pacman configured!"
 }
 
 more_packages() {
-  echo -e "${CYAN}>>> Installing additional packages...${RESET}"
+  echo ">>> Installing additional packages..."
   pacman -S --noconfirm git openssh pacman-contrib networkmanager
-  echo -e "${GREEN}Additional packages installed!${RESET}"
+  echo "Additional packages installed!"
 }
 
 systemd_services() {
-  echo -e "${CYAN}>>> Enabling systemd services...${RESET}"
+  echo ">>> Enabling systemd services..."
   systemctl enable fstrim.timer paccache.timer NetworkManager sshd
-  echo -e "${GREEN}Systemd services enabled!${RESET}"
+  echo "Systemd services enabled!"
 }
 
 bootloader_setup() {
-  echo -e "${CYAN}>>> Installing bootloader...${RESET}"
+  echo ">>> Installing bootloader..."
   bootctl --path=/boot install
   
-  echo -e "${CYAN}>>> Configuring bootloader settings...${RESET}"
+  echo ">>> Configuring bootloader settings..."
   {
     echo default arch
     echo timeout 0
@@ -149,10 +139,10 @@ bootloader_setup() {
     echo editor no
   } >>/boot/loader/loader.conf
   
-  echo -e "${CYAN}>>> Getting UUID for root partition...${RESET}"
+  echo ">>> Getting UUID for root partition..."
   UUID=$(blkid -s UUID -o value "$DISK"2)
 
-  echo -e "${CYAN}>>> Configuring bootloader entry for Arch Linux...${RESET}"
+  echo ">>> Configuring bootloader entry for Arch Linux..."
   {
     echo title Arch Linux
     echo linux /vmlinuz-linux
@@ -160,7 +150,7 @@ bootloader_setup() {
     echo options root=UUID="$UUID" quiet rw
   } >>/boot/loader/entries/arch.conf
   
-  echo -e "${GREEN}>>> Bootloader installed and configured!${RESET}"
+  echo ">>> Bootloader installed and configured!"
 }
 
 base_chroot() {
@@ -186,9 +176,9 @@ base() {
   cp "$0" /mnt/root/install.sh
   arch-chroot /mnt /bin/bash /root/install.sh chroot
 
-  echo -e "${GREEN}Base system setup complete!${RESET}"
+  echo "Base system setup complete!"
 
-  echo -e "${GREEN}>>> Leaving chroot and rebooting"
+  echo ">>> Leaving chroot and rebooting"
   
   umount -R /mnt
   reboot
